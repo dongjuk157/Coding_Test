72d02810dbe36e2509f046482cb167a754dd9247
Donggun Ko <dongjuk157@gmail.com>
2021-07-11 14:19:05 +0900

    210711 boj 2108 (2)

--- Baekjoon/Level12/2108.py
+++ Baekjoon/Level12/2108.py
@@ -8,16 +8,25 @@ for i in range(N):
     count[num + 4000] += 1
     total += num
 
-sa = []
-fa = []
-f = 0
+index = 0
+mid = 0
+mid_chk = False
+min_val = 0
+min_chk = False
+max_val = 0
 for i in range(len(count)):
     if count[i]:
-        sa.extend([i - 4000 for _ in range(count[i])])
-        if f < count[i]:
-            f = count[i]
-            fa = [i - 4000]
-        elif f == count[i]:
-            fa.append(i - 4000)
+        index += count[i]
+        max_val = i - 4000
+        if not mid_chk and index > N // 2:
+            mid_chk = True
+            mid = i - 4000
+        if not min_chk:
+            min_chk = True
+            min_val = i - 4000
+tmp = max(count)
+tmp_arr = [i - 4000 for i in range(len(count)) if count[i] == tmp]
+
+
 # 산술평균, 중앙값, 최빈값, 범위
-print(round(total/N), sa[N//2], fa[1] if len(fa) > 1 else fa[0], sa[-1] - sa[0], sep='\n')
+print(round(total/N), mid, tmp_arr[1] if len(tmp_arr) > 1 else tmp_arr[0] , max_val - min_val, sep='\n')
