63b2806256ab4d0023307bde565ad43765898477
Donggun <35018470+dongjuk157@users.noreply.github.com>
2022-01-10 11:07:32 +0900

    Create 1956.py

---
+++ Baekjoon/Level25/1956.py
@@ -0,0 +1,43 @@
+import sys; input = sys.stdin.readline
+INF = 987654321
+
+def floydWashall(linked_road):
+    V = len(linked_road) - 1 
+    distances = [[INF for _ in range(V + 1)] for _ in range(V + 1)]
+
+    for i in range(1, V + 1):
+        for j in range(1, V + 1):
+            for k in range(1, V + 1):
+                distances[i][j] = min(
+                    distances[i][j],
+                    linked_road[i][k] + linked_road[k][j]     
+                )
+    
+    return distances
+
+
+def main():
+    # 0 입력
+    V, E = map(int, input().split())
+    linked_road = [[INF for _ in range(V + 1)] for _ in range(V + 1)]
+    for _ in range(E):
+        a, b, c = map(int, input().split())
+        linked_road[a][b] = c
+    for i in range(1, V + 1):
+        linked_road[i][i] = 0
+     
+    # 1 모든 도시간 최소 거리 탐색
+    distances = floydWashall(linked_road)
+    
+    # 2 사이클 탐색
+    answer = INF
+    for i in range(1, V + 1):
+        for j in range(i + 1, V + 1):
+            answer = min(answer, distances[i][j] + distances[j][i])
+    # 3 최소 도로 길이 출력
+    if answer >= INF:
+        answer = -1
+    print(answer)
+
+if __name__ == "__main__":
+    main()
